ad52224fd511d2f3e84280d5ac8e39c37bbdc427
Philippe <philippe@Vans-MacBook-Air.local>
2025-06-06 01:18:53 -0700

    args for config path. next, parse config

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use clap::Parser;
+
+/// MyApp – does something cool with a config file
+#[derive(Parser, Debug)]
+#[command(name = "ict_server")]
+#[command(about = "Runs the ICT server", long_about = None)]
+struct Args {
+    /// Path to the configuration file
+    #[arg(
+        short,
+        long,
+        value_name = "PATH-TO-FILE",
+        default_value = "configs/ict_server.toml"
+    )]
+    config: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("Using config file: {}", args.config);
 }
