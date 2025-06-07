85ff4d7bed26280ea0a88e0b46613a31eca07521
Philippe Pascal <>
2025-06-07 10:11:05 -0700

    that should be all commands

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ mod ict_config;
 use ict_config::load_config;
 use ict_server::ict_db::Db;
 
+use crate::ict_args::Operation;
+
 fn main() {
     let args = ict_args::load_args();
     println!("Using config file: {}", args.config);
@@ -19,6 +21,36 @@ fn main() {
 
     let db = Db::new(&settings.database.path).expect("Failed to open DB");
 
-    println!("Total devices: {}", db.count_devices().unwrap());
-    db.print_all_devices().unwrap();
+    match &args.operation {
+        Operation::Register { uuid, public_key } => {
+            println!("Registering new client");
+        }
+        Operation::Authorize { uuid } => {
+            println!("Authorizing a registered client");
+        }
+        Operation::Unauthorize { uuid } => {
+            println!("Unauthorizing a registered client (can be re-authorized)");
+        }
+        Operation::Delete { uuid } => {
+            println!("Deleting a registration (can not be undone)");
+        }
+        Operation::Operate { uuid, message } => {
+            println!("validate the message passed by client, and operate associated relays");
+        }
+        Operation::List_Clients {} => {
+            println!("listing clients");
+        }
+        Operation::Describe_Client { uuid } => {
+            println!("showing status and relays of client");
+        }
+        Operation::Associate_Relay { uuid, relay } => {
+            println!("add relay {} to client {}", relay, uuid);
+        }
+        Operation::Clear_Relays { uuid } => {
+            println!("removing all relays of client {}", uuid);
+        }
+        Operation::Serve { port } => {
+            println!("Starting server on port {}", port);
+        }
+    }
 }
