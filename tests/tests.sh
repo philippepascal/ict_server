9ddda40452d611ac555d6a4d3dc25b6900424df6
philippepascal <philippe@pascalonline.org>
2025-06-18 15:41:49 -0700

    improve testing, cleanup

--- tests/tests.sh
+++ tests/tests.sh
@@ -7,9 +7,35 @@
 #openssl pkcs8 -topk8 -inform PEM -in private_pkcs1.pem -outform PEM -nocrypt -out private_pkcs8.pem
 #openssl pkey -in private_pkcs8.pem -pubout -out public.pem
 
+# Defaults
+HOST="localhost"
+UUID=""
+PAUSE=""
+
+# Parse options
+while getopts "h:u:a" opt; do
+  case $opt in
+    h)
+      HOST="$OPTARG"
+      ;;
+    u)
+      UUID="$OPTARG"
+      ;;
+    a)
+      PAUSE="Y"
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+  esac
+done
+
+echo "HOST is: $HOST"
+
+
 # Configuration
-SERVER_URL="http://localhost:3456/register"   # ← Replace with your actual server URL
-SERVER_URL2="http://localhost:3456/operate"   # ← Replace with your actual server URL
+SERVER_URL="http://$HOST:3456/register"   # ← Replace with your actual server URL
+SERVER_URL2="http://$HOST:3456/operate"   # ← Replace with your actual server URL
 KEY_NAME="mykey"                                # base filename
 KEY_BITS=2048
 
@@ -23,7 +49,9 @@ openssl pkcs8 -topk8 -inform PEM -in "../target/${KEY_NAME}_pkcs1.pem" -outform 
 openssl pkey -in "../target/${KEY_NAME}_pkcs8.pem" -pubout -out "../target/${KEY_NAME}_pub.pem"
 
 # Step 4: Generate a random UUID
-UUID=$(uuidgen)
+if [ -z "$UUID" ]; then
+  UUID=$(uuidgen)
+fi
 
 # Step 5: Prepare the JSON body
 PUBLIC_KEY_CONTENT=$(awk '{ printf "%s\\n", $0 }' "../target/${KEY_NAME}_pub.pem")
@@ -89,12 +117,46 @@ FULL_PAYLOAD=$(jq -n \
 
 echo "full payload $FULL_PAYLOAD"
 
+#this one is supposed to fail (not authorized)
 curl -X POST "$SERVER_URL2" \
      -H "Content-Type: application/json" \
      -d "$FULL_PAYLOAD" \
      -v
 
-cargo run -- -c ict_server.toml authorize -u $UUID
+if [ -z "$PAUSE" ]; then
+  cargo run -- -c ict_server.toml authorize -u $UUID
+else
+  read -n 1 -s -r -p "Paused to allow you to authorize manually with UUID $UUID. Press any key to continue..."
+  echo
+fi
+
+#recreate a token in case authorization was manual and took too much time
+TOKEN=$(oathtool --totp -b "$DECRYPTED_SECRET")
+
+echo "token $TOKEN"
+
+SALT="random_salt_xyz"
+
+JSON_PAYLOAD=$(jq -n \
+  --arg token "$TOKEN" \
+  --arg _salt "$SALT" \
+  '{token: $token, _salt: $_salt}')
+
+echo "payload $JSON_PAYLOAD"
+
+SIGNATURE=$(printf '%s' "$JSON_PAYLOAD" | \
+  openssl dgst -sha256 -sign "../target/${KEY_NAME}_pkcs8.pem" | \
+  base64)
+
+echo "signature $SIGNATURE"
+
+FULL_PAYLOAD=$(jq -n \
+--arg id "$UUID" \
+--arg totp_message "$JSON_PAYLOAD" \
+--arg signature "$SIGNATURE" \
+'{id: $id, totp_message: $totp_message, signature: $signature}')
+
+echo "full payload $FULL_PAYLOAD"
 
 curl -X POST "$SERVER_URL2" \
      -H "Content-Type: application/json" \
